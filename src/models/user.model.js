4c91cf00e380ae61c52ec21c4f3f3b338d7220da
anonys6 <shakibalam998@gmail.com>
2024-09-12 02:11:45 +0530

    added user routes and controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
@@ -88,4 +88,4 @@ userSchema.methods.generateRefreshToken = function () {
     );
 };
 
-export default User = model("User", userSchema);
+export const User = model("User", userSchema);
