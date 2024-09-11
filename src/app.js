7aca251ba547ec121b88f2d7a96db29cc5e050f0
anonys6 <shakibalam998@gmail.com>
2024-09-11 21:48:04 +0530

    project config complete

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(
     express.urlencoded({
         extended: true,
-        limit: "16kb",
+        limit: "16kb", 
     })
 );
 app.use(express.static("public"));
