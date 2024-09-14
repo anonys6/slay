d8a5a541e6f4654db7d20ee965ec646ac6820927
anonys6 <shakibalam998@gmail.com>
2024-09-14 15:13:47 +0530

    postman done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
-// import mongoose from "mongoose";
-// import express from "express";
-// import { DB_NAME } from "./constants";
-
-// const app = express();
-
-// (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//         app.on("error", (error) => {
-//             console.error("ERROR: ", error);
-//             throw error;
-//         });
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(
-//                 `App is listening on http://localhost:${process.env.PORT}`
-//             );
-//         });
-//     } catch (error) {
-//         console.error("ERROR: ", error);
-//         throw error;
-//     }
-// })();
-
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/index.js";
